a50cadbb2050123cdc74020a19a723436c642450
Jack Kuhn <jackakuhn@icloud.com>
2025-11-20 15:33:44 -0600

    Change correct grammar

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-document.quieryslector("#show-login").addEventListener("click",function(){
+document.querySelector("#show-login").addEventListener("click",function(){
     document.querySelector(".popup").classList.add("active");
 })
-document.quieryslector(".popup .close-btn").addEventListener("click",function(){
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
     document.querySelector(".popup").classList.remove("active");
 })
