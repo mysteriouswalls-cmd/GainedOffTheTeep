0e7c79ef3662217b7ee03782209e2faf60895fc5
Jack Kuhn <jackakuhn@icloud.com>
2025-11-20 15:45:47 -0600

    change to maybe match?

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document.querySelector("#show-login").addEventListener("click",function(){
+document.querySelector("show-login").addEventListener("click",function(){
     document.querySelector(".popup").classList.add("active");
 })
 document.querySelector(".popup .close-btn").addEventListener("click",function(){
