b8276d8b8077d5594a3ebef8d18833722adc772c
Jack Kuhn <jackakuhn@icloud.com>
2025-11-22 20:34:23 -0600

    Checking

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document.getElementById("#show-login").addEventListener("click",function(){
+document.getElementById("show-login").addEventListener("click",function(){
     document.querySelector(".popup").classList.add("active");
 })
 document.querySelector(".popup .close-btn").addEventListener("click",function(){
