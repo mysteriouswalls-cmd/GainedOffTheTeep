09cf813c2ace443bb2ec0b7ee9101e95264942b1
Jack Kuhn <jackakuhn@icloud.com>
2025-12-04 15:40:31 -0600

    Might be switching to Node.JS

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
- // const supabase = supabase.createClient('https://oxidikugtxyakkxktkoa.supabase.co', 'sb_publishable_JYbDErJUwIzYn6U42ecZYQ_X7gJrrpc')
+const supabase = window.supabase.createClient('https://oxidikugtxyakkxktkoa.supabase.co', 'sb_publishable_JYbDErJUwIzYn6U42ecZYQ_X7gJrrpc');
 document.addEventListener("DOMContentLoaded",function () {
   document.getElementById("show-login").addEventListener("click",function(){
       document.querySelector(".popup").classList.add("active");
@@ -7,18 +7,16 @@ document.addEventListener("DOMContentLoaded",function () {
   document.querySelector(".popup .close-btn").addEventListener("click",function(){
     document.querySelector(".popup").classList.remove("active");
   });
-
-  // document.getElementById("google-login").addEventListener("click", async function () {
-  //   console.log("Google login clicked");
   
-
-  //   const { data, error } = await supabase.auth.signInWithOAuth({
-  //     provider: "google",
-  //     options: {
-  //       redirectTo: window.location.origin // redirect back to home page
-  //     }
-  //   });
-  // });
+  document.getElementById("google-login").addEventListener("click", async function() {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'github',
+      options: {
+        redirectTo: "https://mysteriouswalls-cmd.github.io/"
+      }
+    });
+  });
+  
 });
 
 
